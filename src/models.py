44a950e0907c0fb7b8c2bb9e7328d0a70c7a821a
viku <viku9358@gmail.com>
2024-01-31 13:17:13 +0530

    add try and except to handle error

--- src/models.py
+++ src/models.py
@@ -4,15 +4,19 @@ import google.generativeai as genai
 
 load_dotenv()
 
-api_key = os.getenv("GOOGLE_API_KEY")
-# Use the API key as needed
-genai.configure(api_key=api_key)
-
 def generate_text(prompt):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(prompt)
+    try:
+        api_key = os.getenv("GOOGLE_API_KEY")
+        # Use the API key as needed
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel('gemini-pro')
+        response = model.generate_content(prompt)
 
-    return response.text
+        return response. Text
+    except Exception as e:
+        # Handle the error appropriately
+        print(f"An error occurred: {e}")
+        return None
 
 def process_image():
     pass
